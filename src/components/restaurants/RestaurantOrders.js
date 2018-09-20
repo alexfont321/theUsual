b0a070c8a05e48367647c3176843bc215b28d8d6
alexfont <alexfont321@gmail.com>
2018-09-19 22:24:39 -0500

    initial styling complete on the order cards

--- src/components/restaurants/RestaurantOrders.js
+++ src/components/restaurants/RestaurantOrders.js
@@ -16,21 +16,46 @@ export default class RestaurantOrder extends Component {
 
         return (
             <React.Fragment>
-                <h1>Orders</h1>
-                <button onClick={() => this.props.history.push(`/group/${correctGroupId}/restaurant/${correctRestaurantId}/add-order`)}>Add an Order</button>
-                {
-                    this.props.orders.map(order => {
-                        return <div className="orders" key={order.id}>
-                            <p>{order.food}</p>
-                            <button
-                                onClick={() => this.props.deleteOrder("orders", `${order.id}`, `${correctRestaurantId}`)}
-                            >Delete</button>
-                            <button id={order.id}
-                            onClick={this.moveToEdit}>Edit</button>
-                        </div>
-                    })
-                }
+                <section className="hero">
+                    <div className="hero-body">
+                        <h1 className="title has-text-black">Orders</h1>
+                    </div>
+                </section>
+                <div className="columns">
+                    <div className="column is-1"></div>
+                        <div className="column is-2">
+                    <button className="button is-rounded is-primary is-outlined"
+                        onClick={() => this.props.history.push(`/group/${correctGroupId}/restaurant/${correctRestaurantId}/add-order`)}>Add an Order</button>
+                    </div>
+                    <div className="column">
+                        <section className="columns is-multiline">
+                            {
+                                this.props.orders.map(order => {
+                                    return <div className="column is-one-quarter" key={order.id}>
+                                            <div className="card">
+                                                <div className="card-header">
+                                                    <p>{order.userId}</p>
+                                                </div>
+                                                <div className="card-content">
+                                                     <p>{order.food}</p>
+                                                </div>
+                                                <div className="card-footer">
+                                
+                                        <button id={order.id} className="card-footer-item button is-paddingless is-inverted is-info"
+                                            onClick={this.moveToEdit}>Edit</button>
+                                            <button className="card-footer-item button is-paddingless is-inverted is-danger"
+                                                     onClick={() => this.props.deleteOrder("orders", `${order.id}`, `${correctRestaurantId}`)}>Delete</button>
+                                            </div>
+                                    </div>
+                                </div>
+                                })
+                            }
+                        </section>
+                    </div>
 
+                    <div className="column is-1"></div>
+
+                </div>
             </React.Fragment>
         )
     }
